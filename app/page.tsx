206cd7ae1f502022eaebce1bd5df9377c4635151
pratapmehta70536 <kirtimanmehta0@gmail.com>
2025-12-07 11:55:58 +0545

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,68 @@
-import Image from "next/image";
+"use client";
+
+import { motion } from "motion/react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="relative my-10 flex flex-col items-center justify-center">
+      <Navbar />
+      
+      
+      
+      <div className="px-4 py-10 md:py-20">
+        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-purple-700 md:text-4xl lg:text-7xl dark:text-slate-950">
+          {"🩺Transform  Healthcare  With  AI  Medical  Voice  Agent"
+            .split(" ")
+            .map((word, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+                transition={{ duration: 0.3, delay: index * 0.1, ease: "easeInOut" }}
+                className="mr-2 inline-block"
+              >
+                {word}
+              </motion.span>
+            ))}
+        </h1>
+        <br />
+        <br />
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.8 }}
+          className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
+        >
+          Provide 24/7 intellignet medical support using conversational AI. Deliver instant, accurate medical assistance, triage symptoms, deliver empathetic care with voice-first automation.<span className="text-red-500 text-2xl">Practically tested</span> AI based <span className="text-red-500 text-2xl">user choice medical platform.</span>
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 1 }}
+          className="relative z-10 mt-8 flex items-center justify-center"
+        >
+          <button className="px-8 py-3 rounded-xl font-semibold shadow-lg bg-gradient-to-r from-blue-600 to-indigo-600 text-white transition-all duration-300 hover:scale-105 hover:shadow-xl">
+            Get Started
+          </button>
+        </motion.div>
+        
+        
+      </div>
     </div>
   );
 }
+
+const Navbar = () => {
+  return (
+    <nav className="flex w-full items-center justify-between  px-4 ">
+      <div className="flex items-center gap-2">
+       <img src="/logo.png" alt="AI Medical Logo" className="w-30 h-25 rounded-lg"/>
+      </div>
+      
+      <div className="flex items-center gap-2"></div>
+      <button className="w-28 rounded-xl px-6 py-3 mb-10 font-semibold bg-gradient-to-r from-purple-600 to-pink-500 text-white shadow-md hover:scale-105 transition-all duration-300">
+        Login
+      </button>
+    </nav>
+  );
+};

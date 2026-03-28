c8c542ab1eb03b8dcb514892e2a37fd2debfed61
pratapmehta70536 <kirtimanmehta0@gmail.com>
2026-03-28 15:03:59 +0545

    update made

--- app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -184,7 +184,7 @@ function MedicalVoiceAgent() {
     <div className="mt-10 p-5 border rounded-3xl bg-secondary relative">
       <div className="bg-yellow-500/10 text-yellow-600 py-2 px-3 sm:px-4 rounded-xl mb-6 text-sm text-justify sm:text-center border border-yellow-500/20 flex flex-col sm:flex-row items-center sm:justify-center gap-2">
         <AlertTriangle className="h-4 w-4 shrink-0" />
-        <p>Don&apos;t forget to make sure having good internet connection and silent environment for flawless communication. Also Keep your device near your vocal zone.</p>
+        <p>Don&apos;t forget to make sure having good internet connection and silent environment for flawless communication. Also Keep your device near your vocal zone. Say "continue" if conversation stucks in middle.</p>
       </div>
       {isGeneratingReport && (
         <div className="absolute inset-0 bg-secondary/80 backdrop-blur-sm z-50 flex flex-col items-center justify-center rounded-3xl">
